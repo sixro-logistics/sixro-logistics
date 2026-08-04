7467e53d12af56ecdf0f4641cad647d2269cdf74
skdysla <skdysla@gmail.com>
2026-08-04 20:05:34 +0900

    chore: 재고 서비스 초기 설정

---
+++ inventory-service/src/main/java/com/sixro/logistics/inventory/InventoryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.sixro.logistics.inventory;
+
+import com.sixro.logistics.common.persistence.config.QuerydslConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@EnableFeignClients
+@Import(QuerydslConfig.class) // common module의 QuerydslConfig를 사용하기 위해 추가
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+}
