ef32ed692ff925c18a39db16ac68a3c9d381135d
skdysla <skdysla@gmail.com>
2026-08-06 14:32:40 +0900

    refactor: QueryConfig 자동 주입 적용 및 메인 클래스 @Import 제거

--- inventory-service/src/main/java/com/sixro/logistics/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/sixro/logistics/inventory/InventoryServiceApplication.java
@@ -1,16 +1,13 @@
 package com.sixro.logistics.inventory;
 
-import com.sixro.logistics.common.persistence.config.QuerydslConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
-@Import(QuerydslConfig.class) // common module의 QuerydslConfig를 사용하기 위해 추가
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
