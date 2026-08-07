bf14540549a04f3996ba32868a475752abf47683
dhLeoKim <win3ctrl@gmail.com>
2026-08-07 19:29:49 +0900

    feat: hub-service 에 redis, kafka 설정 / testcontainers 적용 및 동작 확인을 위한 테스트 코드 추가

--- hub-service/src/test/java/com/sixro/logistics/hub/HubServiceApplicationTests.java
+++ hub-service/src/test/java/com/sixro/logistics/hub/HubServiceApplicationTests.java
@@ -1,11 +1,20 @@
 package com.sixro.logistics.hub;
 
-import org.junit.jupiter.api.Disabled;
+import com.sixro.logistics.common.test.config.KafkaTestContainerConfig;
+import com.sixro.logistics.common.test.config.PostgresTestContainerConfig;
+import com.sixro.logistics.common.test.config.RedisTestContainerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.test.context.ActiveProfiles;
 
-@Disabled
 @SpringBootTest
+@ActiveProfiles("test")
+@ImportTestcontainers({
+        PostgresTestContainerConfig.class,
+        RedisTestContainerConfig.class,
+        KafkaTestContainerConfig.class
+})
 class HubServiceApplicationTests {
 
     @Test
