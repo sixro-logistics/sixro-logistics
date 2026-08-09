1695388d46323b5fffcf876083162c3451f70604
skdysla <skdysla@gmail.com>
2026-08-09 20:09:39 +0900

    feat: 공통 파일 수정

--- inventory-service/src/main/java/com/sixro/logistics/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/sixro/logistics/inventory/InventoryServiceApplication.java
@@ -3,10 +3,8 @@ package com.sixro.logistics.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableFeignClients
 public class InventoryServiceApplication {
 
