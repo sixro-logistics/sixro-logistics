8fe4c94bb25ba5e9168e4ec299c6c747ae6ce5bf
dhLeoKim <win3ctrl@gmail.com>
2026-08-12 11:14:24 +0900

    feat: Hub - app 계층 개발
    - write-behind 구현
    - 시뮬레이션용 물동량 자동 차감 기능 스케줄링, 및 hubStatus 상태 변화 구현
    - Kafka 이벤트 포트

--- hub-service/src/main/java/com/sixro/logistics/hub/hub/application/query/HubInternalQueryService.java
+++ hub-service/src/main/java/com/sixro/logistics/hub/hub/application/query/HubInternalQueryService.java
@@ -5,6 +5,7 @@ import com.sixro.logistics.hub.hub.domain.exception.HubErrorCode;
 import com.sixro.logistics.hub.hub.domain.model.Hub;
 import com.sixro.logistics.hub.hub.domain.repository.HubQueryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +16,11 @@ import java.util.UUID;
 @Transactional(readOnly = true)
 public class HubInternalQueryService {
 
+    public static final String HUB_INFO_INTERNAL = "hub:info:internal";
+
     private final HubQueryRepository hubQueryRepository;
 
+    @Cacheable(cacheNames = HUB_INFO_INTERNAL, key = "#hubId")
     public HubInternalInfo getHub(UUID hubId) {
         Hub hub = hubQueryRepository.findById(hubId)
                 .orElseThrow(() -> new BaseException(HubErrorCode.HUB_NOT_FOUND));
